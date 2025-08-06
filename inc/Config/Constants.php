2b4bbb5a6479c080dca16e92c3d0b0a25d90b881
ValeryZhitnikov <valeryzhitnikov91@gmail.com>
2025-08-06 16:45:52 +0300

    Add constants

---
+++ inc/Config/Constants.php
@@ -0,0 +1,8 @@
+<?php 
+
+namespace ShortLinks\Config;
+
+abstract class Constants
+{
+  public const string POST_TYPE_SHORTLINK_LABEL = 'shortlink';
+}
--- inc/Services/RegisterPostType.php
+++ inc/Services/RegisterPostType.php
@@ -2,12 +2,14 @@
 
 namespace ShortLinks\Services;
 
+use ShortLinks\Config\Constants;
+
 class RegisterPostType
 {
   public static function registerShortLinkType(): void
-  {
-    register_post_type('shortlink', [
-      'label' => 'Short Links',
+  { 
+    register_post_type(Constants::POST_TYPE_SHORTLINK_LABEL, [
+      'label' => Constants::POST_TYPE_SHORTLINK_LABEL,
       'labels' => [
           'name'          => 'Short Links',
           'singular_name' => 'Short Link',
